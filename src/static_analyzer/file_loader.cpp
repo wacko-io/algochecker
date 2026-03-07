03dd20b01b63f712ed30d7847973a4bf0bbf71c5
qu1lkss <KostyaLeon2006@mail.ru>
2026-03-07 18:47:13 +0300

    add: file_loader

--- src/static_analyzer/file_loader.cpp
+++ src/static_analyzer/file_loader.cpp
@@ -1,10 +1,32 @@
 #include "static_analyzer/file_loader.h"
+#include <fstream>
+#include <string>
+
+using namespace std;
 
 namespace algochecker::static_analyzer {
 
-std::string load_source_file(const std::string& path) {
-    (void)path;
-    return {};
-}
+    string load_source_file(const string& path)
+    {
+        ifstream file(path);
+
+        if (!file.is_open())
+        {
+            return "";
+        }
+
+        string line;
+        string code = "";
+
+        while (getline(file, line))
+        {
+            code += line;
+            code += "\n";
+        }
+
+        file.close();
+
+        return code;
+    }
 
 } // namespace algochecker::static_analyzer
